5ed46418cb472748e2ee0f8f9b26aa25e90f8127
crober97 <crunchie2@hotmail.com>
2019-03-06 16:04:29 +0000

    Implemented bottom navigation for fitness to split schedule and diary

--- app/src/main/java/com/example/crobe/primalfitness/FitnessActivity.java
+++ app/src/main/java/com/example/crobe/primalfitness/FitnessActivity.java
@@ -1,92 +1,36 @@
 package com.example.crobe.primalfitness;
 
-import android.annotation.SuppressLint;
-import android.graphics.Color;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
-import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
-import com.squareup.okhttp.OkHttpClient;
-
-import java.net.MalformedURLException;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import android.view.MenuItem;
 
 public class FitnessActivity extends AppCompatActivity {
 
-    private LinearLayout layoutPlans;
-    private MobileServiceClient mClient;
+    android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
 
-    private MobileServiceTable<PlanLinkItem> mLinkTable;
-    private ServiceHandler sh;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = (MenuItem item) -> {
+        switch (item.getItemId()) {
+            case R.id.navigation_diary_fitness:
+                fragmentManager.beginTransaction().replace(android.R.id.content, new DiaryFragment()).commit();
+                return true;
+            case R.id.navigation_schedule_fitness:
+                fragmentManager.beginTransaction().replace(android.R.id.content, new ScheduleFragment()).commit();
+                return true;
+        }
+        return false;
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fitness);
-        sh = new ServiceHandler(this);
 
-        layoutPlans = findViewById(R.id.scheduledPlans);
+        fragmentManager.beginTransaction().replace(android.R.id.content, new DiaryFragment()).commit();
 
-        try {
-            mClient = new MobileServiceClient("https://primalfitnesshonours.azurewebsites.net", this);
-
-            mClient.setAndroidHttpClientFactory(() -> {
-                OkHttpClient client = new OkHttpClient();
-                client.setReadTimeout(20, TimeUnit.SECONDS);
-                client.setWriteTimeout(20, TimeUnit.SECONDS);
-                return client;
-            });
-
-            mLinkTable = mClient.getTable(PlanLinkItem.class);
-
-        } catch (MalformedURLException e) {
-            sh.createAndShowDialog(new Exception("There was an error creating the Mobile Service. Verify the URL"), "Error");
-        } catch (Exception e) {
-            sh.createAndShowDialog(e, "Error");
-        }
-
-        getCreatedPlans();
-    }
-
-    private void getCreatedPlans() {
-        if (mClient == null) {
-            return;
-        }
-
-        @SuppressLint("StaticFieldLeak") AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected Void doInBackground(Void... params) {
-                try {
-
-                    final List<PlanLinkItem> links = mLinkTable.where().field("username").eq(LoginActivity.loggedInUser).execute().get();
-                    runOnUiThread(() -> {
-                        for (PlanLinkItem itemLinks : links) {
-                            addPlanToScreen(itemLinks);
-                        }
-                    });
-                } catch (final Exception e) {
-                    sh.createAndShowDialogFromTask(e, "Error");
-                }
-                return null;
-            }
-        };
-        sh.runAsyncTask(task);
-    }
-
-    public void addPlanToScreen(PlanLinkItem item) {
-        final TextView planOnScreen = new TextView(this);
-        planOnScreen.setText(item.getPlanName());
-        planOnScreen.setTextSize(36);
-        planOnScreen.setBackground(ContextCompat.getDrawable(this, R.drawable.border));
-        planOnScreen.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        planOnScreen.setTextColor(Color.parseColor("#ff000000"));
-        layoutPlans.addView(planOnScreen);
+        BottomNavigationView navigation = findViewById(R.id.navigation_fitness);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
+
